e830ea0192cd60e116c6b7c393c5c52a57605827
Dino Leung <hi@d1n0.xyz>
2026-06-28 16:46:03 +0930

    setup dev env

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+	description = "Development shell for a go-app application with TinyGo";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { nixpkgs, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+			in
+			{
+				devShells.default = pkgs.mkShell {
+					packages = with pkgs; [
+						go
+						gopls
+						gotools
+						go-tools
+						tinygo
+						just
+						watchexec
+					];
+
+					env = {
+						GO111MODULE = "on";
+						GOPROXY = "https://proxy.golang.org,direct";
+					};
+
+					shellHook = ''
+						echo "go-app dev shell: go $(go version | awk '{print $3}'), tinygo $(tinygo version | awk '{print $3}')"
+					'';
+				};
+			});
+}
